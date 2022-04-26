d4a01cfff467e9300f753f3cda857231725a67bf
TiagoTR <tiagorocha.tr16@gmail.com>
2022-04-26 09:41:07 -0300

    criado o card com dados na sua versao estática

--- src/pages/gh-host/index.tsx
+++ src/pages/gh-host/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Header from "../../components/Header";
-import {Container, Corpo, Title} from "../../styles/ghHost";
+import {BotaoCancelar, Card, Container, Corpo, Title} from "../../styles/ghHost";
 import { BiArrowToLeft } from "react-icons/bi";
 
 
@@ -20,6 +20,51 @@ const GHHost = () => {
           </div>
 
           <Title>Hospedagens que solicitei</Title>
+
+          <Card>
+            <div className="corpo">
+              <div className="coluna-dados">
+                <h2>Cidade</h2>
+                <h3>São Paulo</h3>
+                <h3>São Paulo</h3>
+                <h3>São Paulo</h3>
+                <h3>São Paulo</h3>
+                <h3>São Paulo</h3>
+              </div>
+              <div className="coluna-dados">
+                <h2>Chenk-in</h2>
+                <h3>29/05/22</h3>
+                <h3>29/05/22</h3>
+                <h3>29/05/22</h3>
+                <h3>29/05/22</h3>
+                <h3>29/05/22</h3>
+              </div>
+              <div className="coluna-dados">
+                <h2>Chenk-out</h2>
+                <h3>02/06/22</h3>
+                <h3>02/06/22</h3>
+                <h3>02/06/22</h3>
+                <h3>02/06/22</h3>
+                <h3>02/06/22</h3>
+              </div>
+              <div className="coluna-dados">
+                <h2>Valor</h2>
+                <h3>R$ 110,00</h3>
+                <h3>R$ 110,00</h3>
+                <h3>R$ 110,00</h3>
+                <h3>R$ 110,00</h3>
+                <h3>R$ 110,00</h3>
+              </div>
+              {/*<div className="coluna-botoes">
+                <BotaoCancelar>Cancelar</BotaoCancelar> 
+                <BotaoCancelar>Cancelar</BotaoCancelar> 
+                <BotaoCancelar>Cancelar</BotaoCancelar> 
+                <BotaoCancelar>Cancelar</BotaoCancelar> 
+                <BotaoCancelar>Cancelar</BotaoCancelar> 
+              </div>*/}
+            </div>
+          </Card>
+
           <Title>Hospedagens Aceitas</Title>
 
         </Corpo>
--- src/styles/ghHost.ts
+++ src/styles/ghHost.ts
@@ -44,3 +44,84 @@ export const Title = styled.h1`
 
     color: #0B194A;
 `;
+
+export const Card = styled.div`
+margin-top: 20px;
+    background: #FFFFFF;
+    width: 100%;
+
+    box-shadow: 0px 0px 41px rgba(0, 0, 0, 0.15);
+    border-radius: 20px;
+    padding: 10px 20px;
+    
+
+    div.corpo{
+        display: flex;
+        justify-content: space-between;
+        padding-right: 15px;
+        height: 150px;
+        overflow-y: auto;
+       
+        ::-webkit-scrollbar {
+            width: 10px;
+        }
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1; 
+        }
+        ::-webkit-scrollbar-thumb {
+            background: #888; 
+        }
+        ::-webkit-scrollbar-thumb:hover {
+            background: #555; 
+        }
+    }
+    div.coluna-dados{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        gap: 20px;
+        
+        h2{
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 600;
+            font-size: 1rem;
+
+            color: #212529;
+            margin-bottom: 10px;
+        }
+
+        h3{
+            font-family: 'Manrope';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 0.8rem;
+
+            color: #000000;
+        }
+    }
+
+    div.coluna-botoes{
+        display: flex;
+        flex-direction: column;
+        padding-top: 48px;
+        gap: 14px;
+    }
+`;
+
+export const BotaoCancelar = styled.button`
+    background: #E32A51;
+    border-radius: 10px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    text-align: center;
+
+    color: #FFFFFF;
+
+    border: none;
+
+`;

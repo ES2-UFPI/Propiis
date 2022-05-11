c5e671aeae66c74acf24bcee4275b54fad85161f
Samuel <samuelsilvestre2000@gmail.com>
2022-05-10 21:14:45 -0300

    Adicionando modal de feedback

--- src/pages/relatorio/index.tsx
+++ src/pages/relatorio/index.tsx
@@ -5,7 +5,9 @@ import { useState } from "react";
 import { BiArrowToLeft } from "react-icons/bi";
 import { IoStar } from "react-icons/io5";
 import Header from "../../components/Header";
-import {BodyCard, Card, Card2, Container, H1, Lines, Space} from "../../styles/relatorio";
+import {Avaliado, BodyCard, Card, Card2, Container, H1, Lines, Space} from "../../styles/relatorio";
+
+import {MdStar} from "react-icons/md";
 
 const Obrigado = () => {
 
@@ -17,7 +19,9 @@ const Obrigado = () => {
         {lugar:"Maragogi - AL",preco:1500,checkIn:"29/05/22",checkOut:"29/05/22"},
         {lugar:"Maragogi - AL",preco:1500,checkIn:"29/05/22",checkOut:"29/05/22"}
     ]);
+    const[open,setOpen] = useState(false);
 
+    const [stars, setStars] = useState(5);
     return (
         <Container>
             <div className="voltar">
@@ -60,7 +64,7 @@ const Obrigado = () => {
                                 </div>
                                 
                                 <Space>
-                                    <button>
+                                    <button onClick={()=>setOpen(true)}>
                                         <IoStar color="#F6CA2A" size={24}/>
                                         Avaliar hóspede
                                     </button>
@@ -104,6 +108,33 @@ const Obrigado = () => {
                     <h3>R$ 5000,00</h3>
                 </div>
             </Card2>
+            {open?(
+                    <div id="modal-info" className="modal-container">
+                        <div className="modal">
+                        <button className="fechar" onClick={() => setOpen(false)}> X </button>
+                        <h3>Feedback</h3>
+                            <div className="avaliacao">
+                            <Avaliado>
+                                {Array(5).fill(0).map((_, idx) => {
+                                    return idx < stars ? (
+                                        <MdStar color="#F6CA2A" onClick={() => setStars(idx+1)} size={24} />
+                                    ) : (
+                                        <MdStar color="#C1BDAF" onClick={() => setStars(idx+1)} size={24} />
+                                    )
+                                })}
+                            </Avaliado>
+                            </div>
+                        <h3>Comentários</h3>
+                            <div className="comentario">
+                                <textarea placeholder="Deixe um comentário..."></textarea>
+                            </div>
+                        <div className="botaoAvaliar">
+                            <button className="botao-avaliar">Avaliar</button>
+                        </div>
+                        </div>
+                    </div>)
+
+                :null}  
         </Container>
     );
   };
--- src/styles/relatorio.ts
+++ src/styles/relatorio.ts
@@ -24,6 +24,133 @@ export const Container = styled.div`
             margin-top: 3px; 
         }
     }
+    .modal-container{
+        width: 100vw;
+        height: 100vh;
+        background: rgba(0,0,0,0.5);
+
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        z-index: 2000;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+
+        .modal{
+            padding-bottom: 30px;
+            background: white;
+            border-radius: 30px;
+
+            width: 700px;
+            overflow-y: auto;
+            height: 500px;
+            
+            position: relative;
+            display: flex;
+            flex-direction: column;
+
+            p{
+                font-family: 'Manrope';
+                font-style: normal;
+                font-weight: 600;
+                font-size: 1rem;
+                width: 80%;
+               
+                text-align: center;
+
+                color: #000000;
+            }
+
+            h3{
+                margin-left: 60px;
+                font-family: 'Inter';
+                font-style: normal;
+                font-weight: 700;
+                font-size: 1.8rem;
+
+                color: #0B194A;
+                margin-top: 20px;
+            }
+
+            .avaliacao{
+                display: flex;
+                align-items: left;
+
+                width: 200px;
+                height: 50px;
+                
+                margin-top: 20px;
+                margin-left: 60px;
+
+                box-shadow: 0px 6px 4px rgba(0, 0, 0, 0.25);
+            }
+
+            .comentario{
+                display: flex;
+                align-items: left;
+
+                width: 500px;
+                height: 200px;
+                
+                margin-top: 20px;
+                margin-left: 60px;
+
+                box-shadow: 0px 6px 4px rgba(0, 0, 0, 0.25);
+
+                textarea{
+                    width: 100%;
+                    outline: none;
+                    border: none;
+                    padding-left: 10px;
+                    padding-top: 10px;
+                }
+            }
+            .botaoAvaliar{
+                display: flex;
+                width: 100%;
+                justify-content: right;
+
+                padding-right: 30px;
+                .botao-avaliar{
+                    margin-top: 20px;
+                    width: 150px;
+                    height: 50px;
+                    border: none;
+                    background: #49A5E8;
+                    border-radius: 10px;
+
+                    font-family: 'Poppins';
+                    font-style: normal;
+                    font-weight: 400;
+                    font-size: 1.3rem;
+                    color: white;
+
+                    :hover{
+                        opacity: 80%;
+                    }   
+                }
+            }
+        }
+
+        .fechar{
+            position: absolute;
+            top: 0px;
+
+            right: 10px;
+
+            width: 70px;
+            height: 70px;
+            border-radius: 50%;
+            border: 4px solid white;
+            background: white;
+            font-weight: 600;
+            font-size: 1.5rem;
+
+            cursor: pointer;
+        }
+    }
 `; 
 
 
@@ -208,5 +335,18 @@ export const Card2 = styled.div`
         display: flex;
         justify-content: space-between;
         padding-right: 16px;
+    }
+`;
+export const Avaliado = styled.div`
+    background: #FFFFFF;
+    padding: 5px 15px;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    gap: 5px; 
+
+    div#1{
+        background: blue;
+        margin-left: 200px; 
     }
 `;
